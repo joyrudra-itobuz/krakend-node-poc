3b21b90708e6fbbf2b0a8d58b92afd2df820d4b2
Joyrudra Biswas <joyrudra@itobuz.com>
2024-12-13 10:50:56 +0530

    feat : added multiple server instance

---
+++ apps/index.js
@@ -0,0 +1,7 @@
+import server1 from "./app-4000/src/server.js";
+import server2 from "./app-4040/src/server.js";
+import server3 from "./app-4080/src/server.js";
+
+server1();
+server2();
+server3();
