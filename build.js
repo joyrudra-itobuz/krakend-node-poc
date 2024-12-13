21a0ad5190d5ede74f1c3e68fa51a9d3aba5c340
Joyrudra Biswas <joyrudra@itobuz.com>
2024-12-13 12:34:59 +0530

    feat : added support for multiple krakend file

---
+++ build.js
@@ -0,0 +1,44 @@
+import fs from "node:fs/promises";
+
+const path = `${process.cwd()}/krakend`;
+
+console.log(path);
+
+const endpoints = ["server-4000.json", "server-4040.json", "server-4080.json"];
+
+const defaultConfigurations = {
+  version: 3,
+  name: "KrakenD API Gateway",
+  port: 8080,
+  cache_ttl: "3600s",
+  endpoints: [],
+};
+
+async function appendKrakend() {
+  try {
+    const endpointsJSON = await Promise.all(
+      endpoints.map(async (endpoint) => {
+        const data = await fs.readFile(`${path}/${endpoint}`, "utf-8");
+        return JSON.parse(data);
+      })
+    );
+
+    for (const endpoint of endpointsJSON) {
+      defaultConfigurations.endpoints = [
+        ...defaultConfigurations.endpoints,
+        ...endpoint,
+      ];
+    }
+
+    await fs.writeFile(
+      `${path}/krakend.json`,
+      JSON.stringify(defaultConfigurations, null, 2)
+    );
+
+    console.log("Krakend file created successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+appendKrakend().catch(console.error);
